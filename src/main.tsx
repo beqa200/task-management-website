ae1c50ea48de8aa2162421ee4272811aab5e2411
beqa <beqamaisuradze912@gmail.com>
2023-01-21 22:20:06 +0400

    first commit

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import  ReactDOM  from 'react'
+import App from './App'
+
+// @ts-ignore
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
